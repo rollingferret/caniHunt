335d80388faddf12a4cde0df1c0b8c193ad53c15
Peter Joh <johpeter@gmail.com>
2021-09-15 08:05:03 -0600

    stuck

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,9 +2,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
+import productReducer from './product';
+
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  product: productReducer
 });
 
 let enhancer;
